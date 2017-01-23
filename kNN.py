11301ef9d5fac1d6ae6c60de5eb6a4b0ad82511c
Qinghai <zqhnju241@gmail.com>
2017-01-23 17:46:29 -0500

    first classifier

--- kNN.py
+++ kNN.py
@@ -1,11 +1,22 @@
 #K Nearest Neighbors: KNN
 
-from numpy import *
+import numpy as np
 import operator
 from os import listdir
 
 def createDataSet():
-    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [1, 0.1]])
+    group = np.array([[1.0, 1.1], [1.0, 1.0], [0, 0], [1, 0.1]])
     labels = ['A', 'A', 'B', 'B']
     return group, labels
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = np.tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat ** 2
+    sqDistances = sqDiffMat.sum(axis = 1)
+    distances = sqDistances ** 0.5
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range(k):
+
 
