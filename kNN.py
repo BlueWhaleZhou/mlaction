8c3caa96b84f760c1119ab4094dddd6926a77671
Qinghai <zqhnju241@gmail.com>
2017-01-25 14:37:53 -0500

    knn algorithm

--- kNN.py
+++ kNN.py
@@ -13,10 +13,16 @@ def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
     diffMat = np.tile(inX, (dataSetSize, 1)) - dataSet
     sqDiffMat = diffMat ** 2
-    sqDistances = sqDiffMat.sum(axis = 1)
+    sqDistances = sqDiffMat.sum(axis=1)
     distances = sqDistances ** 0.5
-    sortedDistIndicies = distances.argsort()
+    sortedDistIndicies = np.argsort(distances)
     classCount = {}
     for i in range(k):
+        voteIlable = labels[sortedDistIndicies[i]]
+        #count the number of occurrence for each class, if new class appears, the starting number becomes 0
+        classCount[voteIlable] = classCount.get(voteIlable, 0) + 1
+    #return the number of classes of tuples and sort by second element(number of occurrence of each class in descending order)
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
 
 
