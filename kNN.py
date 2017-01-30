a0d60ad26224af4c800507192606cf2399646590
HTDZ <zqhnju241@gmail.com>
2017-01-30 15:29:54 -0500

    dating data processing done

--- kNN.py
+++ kNN.py
@@ -21,20 +21,28 @@ def classify0(inX, dataset, labels, k):
         vote_lable = labels[sorted_dist_indices[i]]
 # count the number of occurrence for each class, if new class appears, the starting number becomes 0
         class_count[vote_lable] = class_count.get(vote_lable, 0) + 1
-# return the number of classes of tuples and sort by second element(number of occurrence of each class in descending order)
+# return the number of classes of tuples and sort by second element
+# (number of occurrence of each class in descending order)
     sortedclasscount = sorted(class_count.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedclasscount[0][0]
 
 def file2matrix(filename):
     fr = open(filename)
     numberoflines = len(fr.readlines())
+    # initialize a matrix with size: number of lines x 3
     return_mat = np.zeros((numberoflines, 3))
     class_label_vector = []
     index = 0
     for line in fr.readlines():
+        '''
+        line = line.strip()
+        list = line.split('\t')
+        return_mat[index, :] = list[0:3]
+        class_label_vector.append(list[-1])
+        '''
         # delete blank spaces at the beginning and the end of each line
         line = line.strip()
-        #
+        # split data values according to \t
         list_from_line = line.split('\t')
         return_mat[index, :] = list_from_line[0:3]
         class_label_vector.append(int(list_from_line[-1]))
