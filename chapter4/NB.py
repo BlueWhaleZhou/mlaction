98a8bc6b2167ed2ec6c9273f894dff27df840911
Qinghai <zqhnju241@gmail.com>
2017-02-28 15:26:50 -0500

    naive bayes classifier

--- chapter4/NB.py
+++ chapter4/NB.py
@@ -20,13 +20,25 @@ def create_vocab_list(dataset):
 
 
 def words2vec(vocab_list, input_set):
-    return_vec = [0] * len(vocab_list) # list of '0'
+    return_vec = [0] * len(vocab_list) # list of '0's
     for word in input_set:
         if word in vocab_list:
             return_vec[vocab_list.index(word)] = 1 # word exist in vocab_list: 1, otherwise: 0
         else:
             print "the word: %s is not in my vocabulary" % word
     return return_vec
+
+
+def train_nb(train_matrix, train_category):
+    num_train_docs = len(train_matrix)
+    num_words = len(train_matrix[0])
+    p_abusive = sum(train_category) / float(num_train_docs)
+    p0_num = np.zeros(num_words)
+    p1_num = np.zeros(num_words)
+    p0_denom = float(0)
+    p1_denom = float(0)
+
+
 
 
 
